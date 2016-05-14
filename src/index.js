b0a32e40f1925555f0ae2136433a4b2b745a2bd6
DerekDoughty <Doughty326@gmail.com>
2016-05-14 15:15:29 -0700

    added key

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import YTsearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
-const API_KEY = 'AIzaSyBiNtcPGPgkdLUag-hOy8ttBKhbuSAV3mk';
+//const API_KEY = 'AIzaSyBiNtcPGPgkdLUag-hOy8ttBKhbuSAV3mk';
+const API_KEY = 'd6e11728aaeb7577fcd2951b4e6c041d';
 
 class App extends Component {
   constructor(props) {
